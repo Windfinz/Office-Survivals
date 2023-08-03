c5c50fe6a1cb3cdc4dadbec1549dbc197f395307
Windfinz <Phongdraco@gmail.com>
2023-08-03 22:33:46 +0700

    map props locations

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public List<GameObject> terrainChunks;
+    public GameObject player;
+    public float checkerRadius;
+    Vector3 noTerrainPosition;
+    public LayerMask terrainMask;
+    PlayerMovement pm;
+
+    void Start()
+    {
+        pm = FindObjectOfType<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        ChunkerChecker();
+    }
+
+    void ChunkerChecker()
+    {
+
+    }
+}
